ab41106278a2d1a708fac0b1b40084655b397ab1
Simone Anthony <simone.anthony1@yahoo.com>
2022-01-29 20:47:12 -0500

    initial components set up and sass imported

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+    <Header/>
+    <Portfolio/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
