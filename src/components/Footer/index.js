51278de132388616a9ba9d4ea37e1e08e2794b58
Simone Anthony <simone.anthony1@yahoo.com>
2022-01-31 22:04:36 -0500

    footer completed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+
 const Footer = () => {
-      return <footer>Footer</footer>;
-    };
-    
-    export default Footer;
+  return (
+    <footer className="d-flex justify-content-center footer">
+      <a href="https://github.com/simone188535">
+        <FontAwesomeIcon icon={faGithub} size="2x" className="my-3 mx-2" />
+      </a>
+      <a href="https://www.linkedin.com/in/simone-anthony-059066136/">
+        <FontAwesomeIcon icon={faLinkedin} size="2x" className="my-3 mx-2" />
+      </a>
+      <a href="https://stackoverflow.com/users/6195136/simone-anthony">
+        <FontAwesomeIcon
+          icon={faStackOverflow}
+          size="2x"
+          className="my-3 mx-2"
+        />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
