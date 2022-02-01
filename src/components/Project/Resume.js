32267e1be4aee0e9a932ea20d6e2813f69076323
Simone Anthony <simone.anthony1@yahoo.com>
2022-01-31 21:31:36 -0500

    Resume page created

--- src/components/Project/Resume.js
+++ src/components/Project/Resume.js
@@ -1,6 +1,30 @@
-
+import resume from "./../../assets/Resume.pdf";
 const Resume = () => {
-    return <section>Resume</section>;
+    return <section className="resume">
+      <div className="mb-2">Download my <a href={resume} download>resume</a></div>
+
+      <h2 className="resume-header">Front-end Proficiencies</h2>
+      <ul>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Ember.js</li>
+        <li>Typescript</li>
+        <li>Bootstrap</li>
+      </ul>
+
+      <h2 className="resume-header">Back-end Proficiencies</h2>
+      <ul>
+        <li>PHP</li>
+        <li>C/C++</li>
+        <li>PGSQL</li>
+        <li>MySQL</li>
+        <li>Node.js</li>
+      </ul>
+    </section>;
   };
   
   export default Resume;
