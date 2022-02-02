b1aaad720b07438e0467b6c8ddaea0d828cf0cab
Simone Anthony <simone.anthony1@yahoo.com>
2022-02-01 20:06:14 -0500

    styling corrected in portfolio page and links added

--- src/components/Project/Portfolio.js
+++ src/components/Project/Portfolio.js
@@ -11,68 +11,145 @@ const Portfolio = () => {
       <div className="portfolio">
         <section className="row">
           <figure className="col-md-6">
-            <a href="https://github.com/simone188535/MySQL-Employee-Tracker">
-              <img
-                src={employeeTracker}
-                alt="Employee Tracker"
-                className="d-block w-100 portfolio-img"
-              />
-            </a>
-            <figcaption>Employee Tracker</figcaption>
+            <img
+              src={employeeTracker}
+              alt="Employee Tracker"
+              className="d-block w-100 portfolio-img"
+            />
+
+            <figcaption>
+              <a
+                href="https://github.com/simone188535/MySQL-Employee-Tracker"
+                className="portfolio-link"
+              >
+                Employee Tracker
+              </a>
+            </figcaption>
           </figure>
           <figure className="col-md-6">
-            <a href="https://simone188535.github.io/Open-Weather-API-Dashboard/">
-              <img
-                src={noteTaker}
-                alt="Note Taker"
-                className="d-block w-100 portfolio-img"
-              />
-            </a>
-            <figcaption>JSON Note Taker</figcaption>
+            <img
+              src={noteTaker}
+              alt="Note Taker"
+              className="d-block w-100 portfolio-img"
+            />
+            <figcaption>
+              <a
+                href="https://frozen-tor-69384.herokuapp.com/"
+                className="portfolio-link"
+              >
+                JSON Note Taker
+              </a>
+            </figcaption>
+            <div>
+              <a
+                href="https://github.com/simone188535/JSON-Note-Taker"
+                className="portfolio-link"
+              >
+                Github
+              </a>
+            </div>
           </figure>
         </section>
         <section className="row">
           <figure className="col-md-6">
-            <a href="https://floating-chamber-62136.herokuapp.com/">
-              <img
-                src={presidentialGallery}
-                alt="Presidential Gallery"
-                className="d-block w-100 portfolio-img"
-              />
-            </a>
-            <figcaption>Presidential Gallery</figcaption>
+            <img
+              src={presidentialGallery}
+              alt="Presidential Gallery"
+              className="d-block w-100 portfolio-img"
+            />
+            <figcaption>
+              <a
+                href="https://floating-chamber-62136.herokuapp.com/"
+                className="portfolio-link"
+              >
+                Presidential Gallery
+              </a>
+            </figcaption>
+            <div>
+              <a
+                href="https://github.com/mattholtmoore/presidential-gallery"
+                className="portfolio-link"
+              >
+                Github
+              </a>
+            </div>
           </figure>
           <figure className="col-md-6">
-            <a href="https://simone188535.github.io/Open-Weather-API-Dashboard/">
+            <a
+              href="https://simone188535.github.io/Open-Weather-API-Dashboard/"
+              className="portfolio-link"
+            >
               <img
                 src={openWeatherDashboard}
                 alt="Open Weather API Dashboard"
                 className="d-block w-100 portfolio-img"
               />
             </a>
-            <figcaption>Open Weather API Dashboard</figcaption>
+            <figcaption>
+              <a
+                href="https://simone188535.github.io/Open-Weather-API-Dashboard/"
+                className="portfolio-link"
+              >
+                Open Weather API Dashboard
+              </a>
+            </figcaption>
+            <div>
+              <a
+                href="https://github.com/simone188535/Open-Weather-API-Dashboard"
+                className="portfolio-link"
+              >
+                Github
+              </a>
+            </div>
           </figure>
         </section>
         <section className="row">
           <figure className="col-md-6">
-            <a href="https://murmuring-peak-24595.herokuapp.com/">
-              <img
-                src={UXUITips}
-                alt="UX UI Tips"
-                className="d-block w-100 portfolio-img"
-              />
-            </a>
-            <figcaption>UX UI Tips</figcaption>
+            <img
+              src={UXUITips}
+              alt="UX UI Tips"
+              className="d-block w-100 portfolio-img"
+            />
+            <figcaption>
+              {" "}
+              <a
+                href="https://murmuring-peak-24595.herokuapp.com/"
+                className="portfolio-link"
+              >
+                UX UI Tips
+              </a>
+            </figcaption>
+            <div>
+              <a
+                href="https://github.com/simone188535/UI-UX-Tips"
+                className="portfolio-link"
+              >
+                Github
+              </a>
+            </div>
           </figure>
           <figure className="col-md-6">
-            <a href="https://simone188535.github.io/Personal-Work-Day-Scheduler/">
-              <img
-                src={workDayScheduler}
-                alt="Work Day Scheduler"
-                className="d-block w-100 portfolio-img"
-              />
-            </a>
-            <figcaption>Work Day Scheduler</figcaption>
+            <img
+              src={workDayScheduler}
+              alt="Work Day Scheduler"
+              className="d-block w-100 portfolio-img"
+            />
+            <figcaption>
+              <a
+                href="https://simone188535.github.io/Personal-Work-Day-Scheduler/"
+                className="portfolio-link"
+              >
+                Work Day Scheduler
+              </a>
+            </figcaption>
+            <div>
+              <a
+                href="https://github.com/simone188535/Personal-Work-Day-Scheduler"
+                className="portfolio-link"
+              >
+                Github
+              </a>
+            </div>
           </figure>
         </section>
       </div>
